97cbd58199410a9909793587f32384844d7fd68c
SergM2014 <weisse011@gmail.com>
2016-07-27 10:11:04 +0300

    in the first aproach slider is ready

--- public/lib/jqueryfreeslider/script.js
+++ public/lib/jqueryfreeslider/script.js
@@ -1,123 +1,139 @@
-//get amount of slider
-var slider_number = document.getElementById('slider').querySelectorAll('.slider_image');
+class Slider {
 
-
-
-
-function toggleImage(id)//спочатку получаемо цифру 1
-{
-    var currentElem = document.getElementById(id);
-    var currentElemHeight = getElemHeight(id);//получаем высоту элемента
-    var elems = currentElem.getElementsByTagName('*');//елкмкнты що маються в теге содержащиго картинку
-    var flag;
-//elem are [div.bottom,a ]
-
-
-    if(currentElem.classList.contains('notdisplayed'))
+    constructor()
     {
+//get amount of slider
+
+        this.slider_number = document.getElementById('slider').querySelectorAll('.slider_image').length;
+
+    }
+
+
+    static toggleImage(id)//спочатку получаемо цифру 1
+    {
+        let currentElem = document.getElementById(id);
+        let currentElemHeight = Slider.getElemHeight(currentElem);//получаем высоту элемента
+        let elems = currentElem.getElementsByTagName('*');//елкмкнты що маються в теге содержащиго картинку
+
+
+        if (currentElem.classList.contains('notdisplayed')) {
 //hide bottom title
-              for(var i=0; i<elems.length; i++){elems[i].classList.add('unvisible');}
+            for (let i = 0; i < elems.length; i++) {
+               elems[i].classList.add('unvisible');
+            }
             //для visibility
 
-            currentElem.style.height="1px";
+            currentElem.style.height = "1px";
             currentElem.classList.toggle('notdisplayed');
 
             //image will be larging(sliding down)
-            for(var i=0;i<=100;i+=5)
-            {
-                (function()
-                {
-                    var pos=i;
-                    setTimeout(function(){currentElem.style.height = (pos/100)*currentElemHeight+1+"px";},pos*5);
-                }
-                )();
+            for (let i = 0; i <= 100; i += 5) {
+                (function () {
+                    let pos = i;
+                    setTimeout(function () {
+                        currentElem.style.height = (pos / 100) * currentElemHeight + 1 + "px";
+                    }, pos * 5);
+                })();
             }
 
 //botom titel elems are shown
 
-        setTimeout(function(){for(var i=0; i<elems.length; i++){elems[i].classList.remove('unvisible'); }},500);
+            setTimeout(function () {
+                for (let i = 0; i < elems.length; i++) {
+                    elems[i].classList.remove('unvisible');
+                }
+            }, 500);
 
 
+        }
+        else { //reduce slider image(sliding up)
+
+            let theHeight = currentElemHeight - 1 + "px";
 
 
+            for (let i = 0; i < elems.length; i++) {
+                elems[i].classList.add('unvisible');
+            }
 
-    }
-    else
-    { //reduce slider image(sliding up)
-
-            var theHeight= currentElemHeight-1+"px";
-
-
-        for(var i=0; i<elems.length; i++){elems[i].classList.add('unvisible');}
-
-            for (var i=100;i>=0;i-=5)
-            {
-                (function()
-                {
-                    var pos=i;
-                    setTimeout(function()
-                    {
-                        currentElem.style.height = (pos/100)*currentElemHeight+"px";
-                        if (pos<=0)
-                        {
+            for (let i = 100; i >= 0; i -= 5) {
+                (function () {
+                    let pos = i;
+                    setTimeout(function () {
+                        currentElem.style.height = (pos / 100) * currentElemHeight + "px";
+                        if (pos <= 0) {
                             currentElem.classList.toggle('notdisplayed');
-                            currentElem.style.height=theHeight;
+                            currentElem.style.height = theHeight;
 
                         }
-                    },1000-(pos*5));
-                }
-                )();
+                    }, 1000 - (pos * 5));
+                })();
             }
 
 
-    }
-
-}
-
-
-
-function getElemHeight(id)
-{
-    var currentElem = document.getElementById(id);
-
-    if(currentElem.classList.contains('notdisplayed'))
-    {
-        currentElem.classList.add('unvisible');
-
-        currentElem.classList.remove('notdisplayed');
-
-        elemHeight = currentElem.clientHeight||currentElem.offsetHeight+5; // Высота
-
-        currentElem.classList.add('notdisplayed');
-
-        currentElem.classList.remove('unvisible');
-    }
-    else
-    {
-        elemHeight = currentElem.clientHeight||currentElem.offsetHeight+5; // Высота
-    }
-    return elemHeight;
-}
-
-function startSliding(now, last)
-{
-    var newnow;
-
-        if(now == slider_number.length ){
-
-            newnow=1;
-        } else {
-            newnow = (Number(now)+1);
         }
-    if(slider_number.length == 1) newnow=1;
+
+    }
 
 
-    if(last!=0)  {toggleImage(last);}
+    static getElemHeight(slider)
+    {
 
-    setTimeout(function(){toggleImage(now);},1000);//запустыть функцию через промежуток
+        let elemHeight;
 
-    setTimeout(function(){startSliding(newnow, now);}, 6000);
+        //let currentElem = document.getElementById(id);
+
+        if (slider.classList.contains('notdisplayed')) {
+
+            slider.classList.add('unvisible');
+
+            slider.classList.remove('notdisplayed');
+
+            elemHeight = slider.clientHeight || slider.offsetHeight + 5; // Высота
+
+            slider.classList.add('notdisplayed');
+
+            slider.classList.remove('unvisible');
+        }
+        else {
+
+            elemHeight = slider.clientHeight || slider.offsetHeight + 5; // Высота
+
+        }
+
+        return elemHeight;
+    }
+
+
+
+   startSliding(now, last)
+   {
+        let newnow;
+//if thelast slider than reset
+        if (now == this.slider_number) {
+            newnow = 1;
+        } else {
+            newnow = (Number(now) + 1);
+        }
+
+//if the only one slider exists than always 1
+        if (this.slider_number == 1) newnow = 1;
+
+//hidr(slideup) the last active slider
+        if (last != 0) {
+            Slider.toggleImage(last);
+        }
+
+        setTimeout(function () {
+            Slider.toggleImage(now);
+        }, 1000);//запустыть функцию через промежуток
+
+        setTimeout(function () {
+            new Slider().startSliding(newnow, now);
+        }, 6000);
+    }
+
 }
 
 
-window.onload = startSliding('1', '0');
+
+window.onload = new Slider().startSliding('1', '0');
