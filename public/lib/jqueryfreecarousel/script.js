58f6c5f8c79911724820e6b8a1f792f4b64a1ba4
SergM2014 <weisse011@gmail.com>
2016-08-02 10:24:04 +0300

    previous variant of js script for carousel is ready

--- public/lib/jqueryfreecarousel/script.js
+++ public/lib/jqueryfreecarousel/script.js
@@ -1,115 +1,94 @@
-var _scroller;
-_scroller = function () { // scroller
-    return{
-        speed:200,/*10,*. /*скорость, чем больше значние, тем медленнее движение*/
-        direct:-1,/* -1 - движение влево, +1 - вправо*/
-        position:0,
-        t:null,
-        // Инициализация скроллера
-        init: function () {
-            var el;
-            // Установка обработчика колесика мыши
-            el = document.getElementById('scroller_container');
-            _scroller.addEvent(el, 'mousewheel', _scroller.wheel);
-            _scroller.addEvent(el, 'DOMMouseScroll', _scroller.wheel);//вариант события прокрутка мышкой для файерфокс
+let intervalFlow ;
+let scrollingSpeed = 200;/*10,*. скорость, чем больше значние, тем медленнее движение*/
+let scrollingDirect = -1;
+let scrollPosition =0;
+let container = document.getElementById('scroller_container');
 
-            _scroller.timer(_scroller.direct); // запускаю скроллер
-        },
 
-        // Обработчик колесика мыши
-        wheel: function (e) {
-            _scroller.stop();
-            e = e ? e : window.event;
 
-            var wheelData = e.detail ? e.detail * -1 : e.wheelDelta / 40;
+class Scroller {
+
+    static  wheel(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            Scroller.stop();
+
+            let wheelData = e.detail ? e.detail * -1 : e.wheelDelta / 40;
 
             // В движке WebKit возвращается значение в 100 раз больше
             if (Math.abs(wheelData) > 100) {
                 wheelData = Math.round(wheelData / 100);
             }
-            //_scroller.scroll(wheelData*10);
-            _scroller.direct=wheelData>0?1:-1;
-            _scroller.timer(_scroller.direct);
-            if (window.event) {
-                e.cancelBubble = true;
-                e.returnValue = false;
-                e.cancel = true;
-            }
-            if (e.stopPropagation && e.preventDefault) {
-                e.stopPropagation();
-                e.preventDefault();
-            }
-            return false;
-        },
 
-        // Функция скроллера
-        scroll: function (wheel) {
-            var div = document.getElementById('scroller_container').firstElementChild;
-            var the_first, the_last, width;
-            _scroller.position += wheel;//add1 pointt causes gradual moovement to the right or to the left
+            scrollingDirect=wheelData>0?1:-1;
+            Scroller.scroll(scrollingDirect);
 
-            //console.log('_scroller_position=>'+_scroller.position);
+        }
+
+        // движение карусели вправо влево
+    static scroll(wheel) {
+
+            let div = container.firstElementChild;
+            let the_first, the_last, width;
+            scrollPosition += wheel;//add1 point causes gradual moovement to the right or to the left
+
+             scrollPosition += wheel;
 
             if (wheel>0) {
-                if (_scroller.position >= 0) { // берем последнюю картинку и вставляем ёё в начало
+                if (scrollPosition >= 0) { // берем последнюю картинку и вставляем ёё в начало
+
                     // В этот момент можно подгружать более левую картинку и удалить последнюю
                     the_first=div;//.firstElementChild; // контейнер с картинками
                     the_last=the_first.lastElementChild; // последняя картинка вместе с анкором
                     width= the_last.firstElementChild.clientWidth; // размер картинки
                     the_first.insertBefore(the_last,the_first.firstElementChild);
-                    _scroller.position-=width;
+                    scrollPosition-=width;
                 }
             }
             else {
+//console.log('wheel is < 0');
                 the_first = div;//.firstElementChild; // контейнер с картинками
 
                 the_last = the_first.firstElementChild; // первая картинка вместе с анкором
                 width = the_last.firstElementChild.clientWidth; // размер картинки
-                if(_scroller.position < -width){ // если картинка ушла влево из зоны видимости переношу её в конец списка
-                    // В этот момент можно подгружать следующую картинку и удалить первую
+                if(scrollPosition < -width){ // если картинка ушла влево из зоны видимости переношу её в конец списка
+
+                // В этот момент можно подгружать следующую картинку и удалить первую
                     the_first.appendChild(the_last);
 
-                    _scroller.position+=width;//пысля того як первый рисунок переставленный назад обнуяеться
-                    //тобто зменшуеться до  -1
-                   // console.log('scroller_position=>=>'+_scroller.position);//schreibt immer -1
+                   scrollPosition+=width;//пысля того як первый рисунок переставленный назад обнуяеться
 
+                    //тобто зменшуеться до  -1
                 }
             }
-            div.style.left = _scroller.position + 'px';
-//console.log(/*'_scroller_position=>'+_scroller.position+*/'    width=>'+width);//_scroller_position=>-133    width=>133
-
-        },
-
-        // Таймер скроллера
-        timer: function (wheel) {//по умолчаинию при загрузке -1
-            _scroller.stop();
-            _scroller.t = setInterval("_scroller.scroll(" + wheel + ");", _scroller.speed);
-            //launch endless cycle
-        },
+            div.style.left = scrollPosition + 'px';
+        }
 
         // Остановка скроллера
-        stop: function () {
-            if (_scroller.t != null) {
-                clearInterval(_scroller.t);
-                _scroller.t = null;
+     static  stop () {
+
+            if (intervalFlow != null) {
+                clearInterval(intervalFlow);
+                intervalFlow = null;
             }
-        },
-
-        // назначить обработчик события
-        addEvent:function(el, evType, fn, useCapture) {
-            if (el.addEventListener) {
-                el.addEventListener(evType, fn, useCapture);
-            }else if (el.attachEvent) {
-                var r = el.attachEvent('on' + evType, fn);
-            }else el['on' + evType] = fn;
         }
-    };
-}();//конец функций которае вносится в переменную _scroller
 
 
-//самовызывающаяся функция
-(function(){setTimeout(_scroller.init,100);})();
+     static init(){
 
-document.getElementById('scroller_container').addEventListener('mousemove', function(){_scroller.stop()});
+        intervalFlow = setInterval(Scroller.scroll.bind(Scroller, scrollingDirect), scrollingSpeed);
+    }
 
-document.getElementById('scroller_container').addEventListener('mouseout', function(){_scroller.timer(_scroller.direct)});
+
+}//end of the lass
+
+
+//setTimeout(scroller.init(), 100);
+container.addEventListener('mousewheel', Scroller.wheel);
+Scroller.stop();
+
+Scroller.init();
+
+container.addEventListener('mousemove', Scroller.stop.bind(Scroller));
+
+container.addEventListener('mouseout', Scroller.init.bind(Scroller) );
